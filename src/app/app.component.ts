964ef5ee33f96665b5c7096938e5ebd4e3e70d6f
BrettyUchoa <brettyuchoa@hotmail.com>
2024-03-05 23:43:30 -0300

    Criando repositorio no Github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'course-carsales';
+  title = 'Bem vindo ao Curso de Frontend  com Angular';
 }
